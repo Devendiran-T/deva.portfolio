689f71162ec9ce8a90720d6b4215ffc197ca0cf8
Devendiran-T <tdevendirandevdevidtamil@gmail.com>
2026-04-02 11:53:23 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-  // Required for GitHub Pages — must match your repo name exactly
-  base: '/portfolio/',
 })
